b51abe57f5eab2387da2580b1bfa177c6a342397
Jesus Soto <soleje2862004@gmail.com>
2025-05-23 19:31:48 -0600

    Version 1

---
+++ src/componentes/App.jsx
@@ -0,0 +1,29 @@
+import NavBar from "./NavBar";
+function App() {
+    return (<>
+    <NavBar/>
+    <div>
+        <h2>Bienvenido a mi pagina Web JSL</h2>
+        <hr />
+        <p>
+            Hora de aventura
+            Llama a tus amigos
+
+            Vamos a tierras muy lejanas
+
+            Con Jake el perro y Finn el humano
+
+            Y diversión siempre tendrás
+
+            ¡Es hora de aventura!
+        </p>
+         <img 
+            src="https://www.clacktv.cl/vtrcontent/media/2022/08/personajes-hora-de-aventura.png" 
+            alt="Imagen de ejemplo"
+            style={{ width: "100%", height: "auto" }} 
+        />
+    </div>
+    </>)
+}
+
+export default App;
