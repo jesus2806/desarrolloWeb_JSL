910b4676dbbb076c5ba68aaf673f855ad2c6bc0a
soleje2862004@gmail.com <soleje2862004@gmail.com>
2025-06-13 21:07:48 -0600

    Agregacion 3

--- src/componentes/NavBar.jsx
+++ src/componentes/NavBar.jsx
@@ -2,56 +2,7 @@ import React from 'react';
 
 function NavBar() {
   return (
-    <nav className="navbar navbar-expand-lg bg-success navbar-dark">
-      <div className="container-fluid">
-        <a className="navbar-brand">Hora de Aventura</a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page">Inicio</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link">Personajes</a>
-            </li>
-            <li className="nav-item dropdown">
-              <ul className="dropdown-menu bg-success">
-                <li>
-                  <a className="dropdown-item text-white">
-                    Bosque de los Hongos
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item text-white">
-                    Tierra de Ooo
-                  </a>
-                </li>
-              </ul>
-            </li>
-          </ul>
-          <form className="d-flex" role="search">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Buscar en Ooo"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-light" type="submit">
-              Buscar
-            </button>
-          </form>
-        </div>
-      </div>
-    </nav>
+    <div></div>
   );
 }
 
